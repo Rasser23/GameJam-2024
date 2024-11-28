88e29e21a71f6749c0a7c31443c42d8c439d4231
Oscshup <51968074+Oscshup@users.noreply.github.com>
2024-11-28 16:55:29 +0100

    update script health

--- GameJam 2024 rigtige/Assets/Scripts/NPC/Fireball.cs
+++ GameJam 2024 rigtige/Assets/Scripts/NPC/Fireball.cs
@@ -4,23 +4,20 @@ public class Fireball : MonoBehaviour
 {
     public Vector2 fireDirection;
     private Rigidbody2D rigidbody;
-    public float moveSpeed;
+    private float moveSpeed;
     private float timeExistet = 0f;
-    private HPmanager hpManager; // Reference to HPmanager
+    private HealthManager healthManager; // Reference to HPmanager
 
     void Start()
     {
         moveSpeed = 200f;
         rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
 
-        // Find HPmanager in the scene
-        if (hpManager == null)
+         // Find HealthManager in the scene
+        healthManager = FindObjectOfType<HealthManager>();
+        if (healthManager == null)
         {
-            hpManager = FindObjectOfType<HPmanager>();
-            if (hpManager == null)
-            {
-                Debug.LogError("HPmanager could not be found in the scene!");
-            }
+            Debug.LogError("HealthManager could not be found in the scene!");
         }
     }
 
@@ -38,16 +35,14 @@ public class Fireball : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Fireball hit the player");
-
-            if (hpManager != null)
+            if (healthManager != null)
             {
-                hpManager.TakeDamage(1); // Reduce health by 1
+                healthManager.TakeDamage(1); // Reduce health by 1
                 DestroyObj(); // Destroy the fireball
             }
             else
             {
-                Debug.LogError("HPmanager is not assigned!");
+                Debug.LogError("healthManager is not assigned!");
             }
         }
     }
